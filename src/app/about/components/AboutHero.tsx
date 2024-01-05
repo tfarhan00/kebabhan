7066d7b36116269bee70713aaa54c0025c1bded2
tfarhan00 <farhanalryan2@gmail.com>
2024-01-05 14:31:16 +0700

    adjust font weight & color

--- src/app/about/components/AboutHero.tsx
+++ src/app/about/components/AboutHero.tsx
@@ -4,7 +4,7 @@ import { skills } from "@/data/skills";
 
 export default function AboutHero() {
   return (
-    <div className="w-full flex flex-col pb-12">
+    <div className="w-full flex flex-col pb-12 font-medium">
       {/* Title */}
       <div className="relative py-4 bg-black flex items-end font-medium justify-between px-4 h-32 text-sm whitespace-nowrap"></div>
 
@@ -12,7 +12,7 @@ export default function AboutHero() {
       <div className="flex flex-col items-start md:flex-row md:items-center gap-3 justify-between pt-10 px-4 md:px-0 w-full">
         <div className="flex flex-col items-start font-medium">
           <h2 className="text-lg">Tengku Farhan</h2>
-          <p className="text-black/50">@kebabhan</p>
+          <p className="text-neutral-400">@kebabhan</p>
         </div>
         <SpotifyStats />
       </div>
@@ -22,7 +22,7 @@ export default function AboutHero() {
         <div className="font-medium  rounded-full px-3 py-1 text-sm flex items-center gap-2 bg-blue-50 text-blue-500">
           <p>At a glance</p>
         </div>
-        <p className="text-neutral-500">
+        <p className="text-neutral-400">
           Hi there! I&apos;m a frontend engineer building{" "}
           <a
             href="https://unbound-studio.vercel.app/"
@@ -39,7 +39,7 @@ export default function AboutHero() {
         <div className="font-medium rounded-full px-3 py-1 text-sm flex items-center gap-2 bg-blue-50 text-blue-500">
           <p>Brief</p>
         </div>
-        <p className="text-neutral-500">
+        <p className="text-neutral-400">
           2+ years of experience in web app development with a focus on creating
           visually appealing and user-friendly websites for clients in various
           industries. Strong project management skills, ability to effectively
@@ -48,7 +48,7 @@ export default function AboutHero() {
       </div>
 
       {/* Experience */}
-      <div className="w-full flex flex-col gap-6 pt-16 font-medium px-4 md:px-0 items-start">
+      <div className="w-full flex flex-col gap-6 pt-16 px-4 md:px-0 items-start">
         <div className="font-medium  rounded-full px-3 py-1 text-sm flex items-center gap-2 bg-blue-50 text-blue-500">
           <p>Experience</p>
         </div>
@@ -60,15 +60,13 @@ export default function AboutHero() {
             >
               <div className="flex flex-col items-start gap-2 text-black w-full">
                 <div className="w-full flex flex-col md:flex-row gap-2 items-start md:items-center justify-between">
-                  <h3 className="font-medium text-base md:text-lg">
-                    {experience.title}
-                  </h3>
-                  <p className="text-sm md:text-base text-black/50">
+                  <h3 className="text-base md:text-lg">{experience.title}</h3>
+                  <p className="text-sm md:text-base text-neutral-400">
                     {experience.company}
                   </p>
                 </div>
 
-                <p className="text-sm text-black/50">{experience.date}</p>
+                <p className="text-sm text-neutral-400">{experience.date}</p>
               </div>
             </div>
           ))}
@@ -76,23 +74,25 @@ export default function AboutHero() {
       </div>
 
       {/* Education */}
-      <div className="flex flex-col gap-6 pt-16 font-medium px-4 md:px-0 items-start">
-        <div className="font-medium  rounded-full px-3 py-1 text-sm flex items-center gap-2 bg-blue-50 text-blue-500">
+      <div className="flex flex-col gap-6 pt-16 px-4 md:px-0 items-start">
+        <div className="font-medium rounded-full px-3 py-1 text-sm flex items-center gap-2 bg-blue-50 text-blue-500">
           <p>Education</p>
         </div>
         <div className="flex flex-col items-start gap-10 w-full">
           <div className="flex items-baseline gap-4 md:gap-6 w-full">
             <div className="flex flex-col items-start gap-2 text-black w-full">
               <div className="w-full flex flex-col md:flex-row gap-2 items-start md:items-center justify-between">
-                <h3 className="font-medium text-base md:text-lg">
+                <h3 className="text-base md:text-lg">
                   Frontend Developer Bootcamp
                 </h3>
-                <p className="text-sm md:text-base text-black/50">
+                <p className="text-sm md:text-base text-neutral-400">
                   Kawah Edukasi
                 </p>
               </div>
 
-              <p className="text-sm text-black/50">July 2022 - August 2022</p>
+              <p className="text-sm text-neutral-400">
+                July 2022 - August 2022
+              </p>
             </div>
           </div>
         </div>
@@ -109,7 +109,7 @@ export default function AboutHero() {
               key={skill.link}
               href={skill.link}
               target="_blank"
-              className="flex items-center gap-2 px-2.5 border text-neutral-500 hover:bg-neutral-100 py-0.5 text-sm rounded-full"
+              className="flex items-center gap-2 px-2.5 border text-neutral-400 hover:bg-neutral-100 py-0.5 text-sm rounded-full"
             >
               {skill.name}
             </a>
