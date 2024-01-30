a29d9509bf7e08830dff4c109a06bf4ab976b62c
tfarhan00 <farhanalryan2@gmail.com>
2024-01-31 05:12:45 +0700

    update social

--- src/app/_components/Footer.tsx
+++ src/app/_components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <div className="w-full py-2 mb-16 flex items-center justify-between px-4 md:px-0 text-sm text-black/90 dark:text-white/90">
+    <div className="w-full py-2 mb-20 flex items-center justify-between px-4 md:px-0 text-sm text-black/90 dark:text-white/90">
       <p>2023 &copy; kebabhan</p>
       <div className="flex items-center gap-2">
         <a
@@ -12,19 +12,19 @@ export default function Footer() {
         </a>
         <span>-</span>
         <a
-          href="https://www.instagram.com/kebabhan_/"
-          target="_blank"
-          className="hover:underline"
-        >
-          instagram
-        </a>
-        <span>-</span>
-        <a
           href="https://www.linkedin.com/in/tengkufarhan/"
           target="_blank"
           className="hover:underline"
         >
           linkedin
+        </a>
+        <span>-</span>
+        <a
+          href="https://twitter.com/kebabhan_"
+          target="_blank"
+          className="hover:underline"
+        >
+          X (twitter)
         </a>
       </div>
     </div>
